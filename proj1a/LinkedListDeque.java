290496d5b26d321893009a8a70f08e8b4fe96bcc
Jenny Wang <jennysihua@gmail.com>
2018-08-07 10:36:52 -0400

    add LinkedListDeque methods

--- proj1a/LinkedListDeque.java
+++ proj1a/LinkedListDeque.java
@@ -1,63 +1,84 @@
-//Implement with int first - generics later.
+//It appears that T can only be defined in instance creation.
+//This makes sense, no need for generics for static variables.
+//Why does type erasure occur?
 
-public class LinkedListDeque {
+public class LinkedListDeque<T> {
     int size;
-    int sentinel;
-
-    public LinkedListDeque() {
-        size = 0;
-    }
 
     public class Node {
         public Node prev;
-        public int item;
+        public T item;
         public Node next;
 
-        public Node(Node p, int i, Node n) {
+        public Node(Node p, T i, Node n) {
             prev = p;
             item = i;
             next = n;
         }
-
     }
 
-    public void addFirst(int item) {
-        //Add first node here
+    public Node sentinel = new Node(null, null, null);
+
+    public LinkedListDeque() {
+        size = 0;
+        sentinel.prev = sentinel;
+        sentinel.next = sentinel;
+    }
+
+    public void addFirst(T item) {
+        Node first = new Node(sentinel, item, sentinel.next);
+        sentinel.next.prev = first;
+        sentinel.next = first;
         size ++;
     }
 
-    public void addLast(int item) {
-        //Add last node here
+    public void addLast(T item) {
+        Node last = new Node(sentinel.prev, item, sentinel);
+        sentinel.prev.next = last;
+        sentinel.prev = last;
         size ++;
     }
 
     public boolean isEmpty() {
-        return false;
+        if(sentinel.next == sentinel) return false;
+        return true;
     }
 
     public int size() {
-        return 0;
+        return size;
     }
 
     public void printDeque() {
-
+        String deque = "";
+        Node p = sentinel;
+        while (p.next != sentinel) {
+            deque += p.item.toString();
+        }
+        System.out.println(deque);
     }
 
-    //Why does type erasure occur?
-    public int removeFirst() {
-        int item = (int) new Object();
-        return item;
+    public T removeFirst() {
+        T first = sentinel.next.item;
+        sentinel.next = sentinel.next.next;
+        sentinel.next.prev = sentinel;
+        return first;
     }
 
-    public int removeLast() {
-        int item = (int) new Object();
-        return item;
+    public T removeLast() {
+        T last = sentinel.prev.item;
+        sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
+        return last;
     }
 
-    public int get(int index) {
+    public T get(int index) {
         int item = (int) new Object();
-        return item;
-        // return null if doesn't exist.
+        Node p = sentinel;
+        while (p.next != sentinel && index > 0) {
+            p = p.next;
+        }
+        if (index == 0) return p.item;
+        return null;
     }
 
 }
